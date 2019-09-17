d0c3c768cf54d16c8929cdb573ab9f66b06fc89a
JefferssonSemin <jeffesemin@hotmail.com>
2019-09-17 17:52:13 -0300

    alterado comentario

--- src/trabalho1febe/Trabalho1Febe.java
+++ src/trabalho1febe/Trabalho1Febe.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabalho1febe;
 
 import java.util.ArrayList;
@@ -11,7 +6,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Eliton
+ * @author Jeffersson semin
  */
 public class Trabalho1Febe {
 
